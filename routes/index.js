227b402e6b7edc8704b7ff4f2001ff5a5ed7db04
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-06-21 19:45:48 -0300

    feat: create validation for login - req4

--- routes/index.js
+++ routes/index.js
@@ -19,10 +19,21 @@ router.get('/talker/:id', async (req, res) => {
     return res.status(200).send(talkerId);
 });
 
-router.post('/login', async (req, res) => {
+router.post('/login', (req, res) => {
     const { email, password } = req.body;
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    
+    if (!email) return res.status(400).send({ message: 'O campo "email" é obrigatório' });
+    if (!regex.test(email)) {
+        return res.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"' });
+    }
+    if (!password) return res.status(400).send({ message: 'O campo "password" é obrigatório' });
+    if (password.length < 6) {
+        return res.status(400).send({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+    }
+
     const token = services.generateToken(email, password);
-    res.status(200).send({ token });
+    return res.status(200).send({ token });
 });
 
 module.exports = router;
